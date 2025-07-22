c076d5f93289c86ffaed8b865d1c2b31a2470cba
VvieyAB <ndicuvictoria@gmail.com>
2025-07-22 23:33:20 +0300

    sql-adavanced joint

---
+++ database-adv-script/README.md
@@ -0,0 +1,8 @@
+This script illustrates:
+
+INNER JOIN for linked records only.
+
+LEFT JOIN for all records from the left table with optional right table data.
+
+FULL OUTER JOIN (simulated) for a complete view of all records, whether linked or not.
+
---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,43 @@
+USE airbnb_clone;
+
+--  INNER JOIN – All bookings with their respective users:
+SELECT 
+    Booking.booking_id,
+    Booking.property_id,
+    Booking.start_date,
+    Booking.end_date,
+    User.user_id,
+    User.first_name,
+    User.last_name,
+    User.email
+FROM 
+    Booking
+INNER JOIN 
+    User ON Booking.user_id = User.user_id;
+
+--  LEFT JOIN – All properties and their reviews, including those without reviews:
+SELECT 
+    Property.property_id,
+    Property.name,
+    Property.location,
+    Review.review_id,
+    Review.rating,
+    Review.comment
+FROM 
+    Property
+LEFT JOIN 
+    Review ON Property.property_id = Review.property_id;
+
+-- FULL OUTER JOIN – All users and all bookings, even if not linked
+SELECT 
+    User.user_id,
+    User.first_name,
+    User.last_name,
+    Booking.booking_id,
+    Booking.property_id,
+    Booking.start_date,
+    Booking.end_date
+FROM 
+    User
+FULL OUTER JOIN 
+    Booking ON User.user_id = Booking.user_id;
